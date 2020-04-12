6752847082558e8eac307ac3d4069b6809639ac9
Victor Rosario <vjrosario@hotmail.com>
2020-04-12 14:51:35 -0400

    complete kubectl code

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -6,13 +6,22 @@
 # This is your Docker ID/path
 # dockerpath=<>
 
+export dockerpath=victorrosario/opermachinelearning
+
 # Step 2
 # Run the Docker Hub container with kubernetes
+
+kubectl run machinelearningmicro --image=$dockerpath --port=80 --labels="app=machinelearningmicro"
 
 
 # Step 3:
 # List kubernetes pods
 
+kubectl get pods
+
 # Step 4:
 # Forward the container port to a host
+
+export POD_NAME=$(kubectl get pod -l app=machinelearningmicro -o jsonpath="{.items[0].metadata.name}")
+kubectl port-forward $POD_NAME 8000:80
 
